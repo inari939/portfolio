338a5275e5a3fc18387d94f41e313e98fc547081
alexandra ramirez <alexandraramirez939@gmail.com>
2023-10-11 11:45:28 -0300

    problema del form solucionado, buscando maneras de redirigir la pagina a la que manda al usuario luego de enviar info en el form

--- js/scripts.js
+++ js/scripts.js
@@ -58,3 +58,21 @@ setInterval(function () {
     rain()
 }, 20)
 
+
+
+//formulario//
+
+
+// https://api.jquery.com/jQuery.ajax
+$.ajax({
+    method: 'POST',
+    url: 'https://formsubmit.co/ajax/your@email.com',
+    dataType: 'json',
+    accepts: 'application/json',
+    data: {
+        name: "FormSubmit",
+        message: "I'm from Devro LABS"
+    },
+    success: (data) => console.log(data),
+    error: (err) => console.log(err)
+});

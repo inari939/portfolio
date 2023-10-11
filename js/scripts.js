9be41c07918d1fdaf468828531f0d625e64dfae7
alexandra ramirez <alexandraramirez939@gmail.com>
2023-10-11 11:57:04 -0300

    probando otras alternativas

--- js/scripts.js
+++ js/scripts.js
@@ -60,19 +60,3 @@ setInterval(function () {
 
 
 
-//formulario//
-
-
-// https://api.jquery.com/jQuery.ajax
-$.ajax({
-    method: 'POST',
-    url: 'https://formsubmit.co/ajax/alexandraramirez939@gmail.com',
-    dataType: 'json',
-    accepts: 'application/json',
-    data: {
-        name: "FormSubmit",
-        message: "I'm from Devro LABS"
-    },
-    success: (data) => console.log(data),
-    error: (err) => console.log(err)
-});
